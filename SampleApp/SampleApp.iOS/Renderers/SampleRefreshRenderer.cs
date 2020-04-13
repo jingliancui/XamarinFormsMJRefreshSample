18879dd7cc1addb80e06064156c915fd727877c5
jingliancui <ericcui@qq.com>
2020-04-12 23:34:08 -0700

    add source code

---
+++ SampleApp/SampleApp.iOS/Renderers/SampleRefreshRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SampleApp.Controls;
+using SampleApp.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using MJRefresh;
+using System.Threading.Tasks;
+
+[assembly: ExportRenderer(typeof(SampleRefresh), typeof(SampleRefreshRenderer))]
+namespace SampleApp.iOS.Renderers
+{
+    public class SampleRefreshRenderer: ViewRenderer<SampleRefresh, UIKit.UIScrollView>
+    {
+        private MJRefreshNormalHeader header;
+        private UIKit.UIScrollView view;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<SampleRefresh> e)
+        {
+            if (view == null)
+            {
+                view = new UIKit.UIScrollView();
+            }
+            if (header == null)
+            {
+                header = new MJRefreshNormalHeader
+                {
+                    RefreshingBlock = new MJRefreshComponentAction(async () =>
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(3d));
+                        view.Mj_header().EndRefreshing();
+                    })
+                };
+            }
+            view.Mj_header(header);
+            SetNativeControl(view);
+        }
+    }
+}
